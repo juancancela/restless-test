9995554cf739adcb3332e2f0a30132cd4d019535
juancancela <cancela.juancarlos@gmail.com>
2021-08-28 01:46:01 -0300

    removed uneeded log

--- routes/root.router.js
+++ routes/root.router.js
@@ -5,7 +5,6 @@ const { log, error } = console;
 
 router.use(async (req, res) => {
   try {
-    log("[route.root]::executing social network service");
     const response = await socialNetworkService.get();
     log("[route.root]::executed social network service with following response: ", response);
     res.status(200).send(response);
